cecf62ce3bcda49ac02125aa7976dd34b7c2c9ca
JustDalo <shishlodan@mail.ru>
2021-10-08 21:25:06 +0300

    programmer book

--- src/by/bsuir/lab1/module2/Books/Main.java
+++ src/by/bsuir/lab1/module2/Books/Main.java
@@ -1,7 +1,5 @@
 package by.bsuir.lab1.module2.Books;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
         Book book1 = new Book("BookTitle1", "Author1", 1);
@@ -9,8 +7,16 @@ public class Main {
         Book book3 = new Book("BookTitle1", "Author1", 1);
         Book book4 = null;
         System.out.println(book1.toString());
-        System.out.println(book1.equals(book4));
+        System.out.println(book1.equals(book3));
+        System.out.println(book1.hashCode());
 
-        
+        ProgrammerBook programmerBook1 = new ProgrammerBook("ProgrammerTitle1", "ProgrammerAuthor1", 4, "Java",1);
+        ProgrammerBook programmerBook2 = new ProgrammerBook("ProgrammerTitle2", "ProgrammerAuthor2", 5, "Java",1);
+
+        System.out.println(programmerBook1.toString());
+        System.out.println(programmerBook1.equals(programmerBook2));
+        System.out.println(programmerBook1.hashCode());
+
+
     }
 }
---
+++ src/by/bsuir/lab1/module2/Books/ProgrammerBook.java
@@ -0,0 +1,57 @@
+package by.bsuir.lab1.module2.Books;
+
+import java.util.Objects;
+
+public class ProgrammerBook extends Book{
+    private String language;
+    private int level;
+
+    public ProgrammerBook(String title, String author, int price, String language, int level) {
+        super(title, author, price);
+        this.language = language;
+        this.level = level;
+    }
+
+    public void setLanguage(String language){
+        this.language = language;
+    }
+
+    public String getLanguage(){
+        return language;
+    }
+
+    public void setLevel(int level){
+        this.level = level;
+    }
+
+    public int getLevel(){
+        return level;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+
+        if (!(object instanceof ProgrammerBook) || !super.equals(object)) {
+            return false;
+        }
+
+        ProgrammerBook programmerBook = (ProgrammerBook) object;
+        return level == programmerBook.level &&
+                Objects.equals(programmerBook.language, this.language);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(super.hashCode(), language, level);
+    }
+
+    @Override
+    public String toString(){
+        String returnString = super.toString();
+        returnString = returnString + language;
+        return returnString;
+    }
+}
