616a2fa7e1e76389260659e4671724680ba837df
JustDalo <shishlodan@mail.ru>
2021-10-08 21:15:19 +0300

    override method tostring equals hashcode

--- src/by/bsuir/lab1/module2/Books/Book.java
+++ src/by/bsuir/lab1/module2/Books/Book.java
@@ -1,8 +1,73 @@
 package by.bsuir.lab1.module2.Books;
 
+import java.util.Objects;
+
 public class Book {
     private String title;
     private String author;
     private int price;
     private static int edition;
+
+    public Book(String title, String author, int price){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setPrice(int price){
+        this.price = price;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public static void setEdition(int value){
+        edition = value;
+    }
+
+    public static int getEdition(){
+        return  edition;
+    }
+
+    @Override
+    public String toString() {
+        return author + " ( " + title + " ) ";
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == this) {
+            return true;
+        }
+        else if (!(object instanceof Book)) {
+            return false;
+        }
+
+        Book book = (Book) object;
+        return price == book.price
+                && (title == book.title || title != null && title.equals(book.getTitle()))
+                && (author == book.author || author != null && author.equals(book.getAuthor()));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.title, this.author, this.price);
+    }
 }
--- src/by/bsuir/lab1/module2/Books/Main.java
+++ src/by/bsuir/lab1/module2/Books/Main.java
@@ -1,4 +1,16 @@
 package by.bsuir.lab1.module2.Books;
 
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book("BookTitle1", "Author1", 1);
+        Book book2 = new Book("BookTitle2", "Author2", 2);
+        Book book3 = new Book("BookTitle1", "Author1", 1);
+        Book book4 = null;
+        System.out.println(book1.toString());
+        System.out.println(book1.equals(book4));
+
+        
+    }
 }
